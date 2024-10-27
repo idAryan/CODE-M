2eb15ac3c319a653d3a501ba371c56de8d283479
IU <itsmeanushka18@gmail.com>
2024-10-27 18:06:25 +0530

    my code

--- 1_age.py
+++ 1_age.py
@@ -1,2 +1,5 @@
 # Write a python code to take input birth year and display how much
 # year old is the person in year 2024
+
+z=int(input("enter the year of birth"))
+print('you are',2024-z , "year old")
