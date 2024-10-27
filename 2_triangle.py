1e13c60e6d471703c54ef10300f691131440cfad
IU <itsmeanushka18@gmail.com>
2024-10-27 19:45:17 +0530

    hi

--- 2_triangle.py
+++ 2_triangle.py
@@ -1,1 +1,4 @@
 # take input length and height of triangle and print its area
+a=int(input("enter the length"))
+b=int(input("enter the height"))
+print("the area of the triangle is", 1/2*a*b)
--- 3_isoceles.py
+++ 3_isoceles.py
@@ -2,3 +2,16 @@
 # also check the validity of triangle if triangle is possible or not
 # eg base=8 equal_side=5 area=12 units sq
 # try it with your own in any language
+import math
+a=int(input("enter the length of side "))
+b=int(input("enter the base "))
+m=a**2-(b/2)**2
+n=math.sqrt(m)
+area=1/2*b*n
+print("area of the triangle is" ,area)
+
+if a+a>b:
+    print("your triangle is valid")
+else:
+    print("your triangle is not valid")
+
